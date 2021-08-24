1f47f53d3ba673dba40af1257c02e6fbde99713b
Gilson Reis <gilsonc.reis@gmail.com>
2021-08-24 02:18:01 -0300

    Auth Login is Done

--- api/src/Http/Actions/Users/Login.php
+++ api/src/Http/Actions/Users/Login.php
@@ -2,25 +2,38 @@
 
 namespace App\Http\Actions\Users;
 
+use Firebase\JWT\JWT;
+use App\Traits\JsonResponse;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use App\Traits\JsonResponse;
+use App\Repositories\Interfaces\UserRepositoryInterface;
 
 class Login
 {
-    private $em;
-
     use JsonResponse;
+
+    private $em;
+    private $userRepository;
 
     public function __construct(ContainerInterface $container)
     {
         $this->em = $container->get('em');
+        $this->userRepository = $container->get(UserRepositoryInterface::class);
+        $this->userRepository->setEntityManager($this->em);
     }
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args = []): ResponseInterface
     {
         $input = $request->getParsedBody();
-        return $this->asJson($input, $response);
+
+        $result = $this->userRepository->auth($input);
+
+        if ($result['status'] === 'success') {
+            $token = JWT::encode($result, $_ENV['SECRET_TOKEN'], "HS256");
+            $result['data']['token'] = $token;
+        }
+
+        return $this->asJson($response, $result);
     }
 }
