b391d907d6d94b77135e71baea99c90e85a93b24
Gilson Reis <gilsonc.reis@gmail.com>
2021-08-24 02:58:23 -0300

    Retrive all transactions sums or filtered by store

--- api/src/Http/Actions/Transactions/Retrieve.php
+++ api/src/Http/Actions/Transactions/Retrieve.php
@@ -7,6 +7,7 @@ use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use App\Repositories\Interfaces\TransactionRepositoryInterface;
+use DateTimeImmutable;
 
 final class Retrieve
 {
@@ -26,19 +27,65 @@ final class Retrieve
     {
         $store = null;
 
+        $rows = $this->transactionRepository->getTransactionAllOrByStore();
         if (!is_null($store_id)) {
             $result = $this->em->setEntity(Store::class)->fetchOne($store_id) ?? [];
-            
-            if (!empty($result)) {
-                $store = new Store();
-                $store->setName($result['name'])
-                        ->setOwner($result['owner'])
-                        ->setId($result['id']);
+
+            if (!$result) {
+                return $this->asJson($response, [
+                    'status' => 'fail',
+                    'data' => [
+                        'title' => 'Loja não encontrada!'
+                    ]
+                ]);
             }
+
+            $store = new Store();
+            $store->setName($result['name'])
+                    ->setOwner($result['owner'])
+                    ->setId($result['id']);
+            
+            $rows = $this->transactionRepository->getTransactionAllOrByStore($store);
         }
 
-        $rows = $this->transactionRepository->getTransactionAllOrByStore($store);
-        
-        return $this->asJson($response, $rows);
+        $data = [
+            'status' => 'success',
+            'data' => [
+                'transactions' => [],
+                'total' => "R$ 0,00",
+                'store' => !empty($store) ? $store->toArray() : null
+            ]
+        ];
+
+        if (count($rows) > 0) {
+            $result = [];
+            $total = 0;
+            foreach ($rows as $i => $row) {
+                $result[$i] = $row;
+
+                switch ($row['signal']) {
+                    case "+":
+                        $total += $row['price'];
+                        break;
+                    case "-":
+                        $total -= $row['price'];
+                        break;  
+                }
+
+                $result[$i]['transaction_date'] = (new DateTimeImmutable($row['transaction_date']))->format("d/m/Y \à\s H:i:s");
+                $result[$i]['price'] = "R$ " . number_format($row['price'], 2, ",", ".");
+            }
+
+            $data = [
+                'status' => 'success',
+                'data' => [
+                    'transactions' => $result,
+                    'total' => "R$ " . number_format($total, 2, ",", "."),
+                    'store' => !empty($store) ? $store->toArray() : null
+                ]
+            ];
+        }
+
+        return $this->asJson($response, $data);
     }
 }
